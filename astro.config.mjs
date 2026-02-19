25108e82d72335cc28262cdf4987bd627832a792
Kaique Nogueira <kaiquenogueir@gmail.com>
2026-02-19 18:48:48 -0300

    feat: Update site URLs to include www and configure Vercel build command and output directory.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    site: 'https://julianacapucci.com.br',
+    site: 'https://www.julianacapucci.com.br',
     output: 'static',
     integrations: [sitemap()],
     build: {
